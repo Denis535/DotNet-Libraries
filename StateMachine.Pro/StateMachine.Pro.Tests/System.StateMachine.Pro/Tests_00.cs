8415368e42ed30fd7cb6644d5eed092cade7eeb0
Denis535 <dddenisss35@gmail.com>
2025-12-14 15:12:10 +0200

    StateMachine.Pro

--- StateMachine.Pro/StateMachine.Pro.Tests/System.StateMachine.Pro/Tests_00.cs
+++ StateMachine.Pro/StateMachine.Pro.Tests/System.StateMachine.Pro/Tests_00.cs
@@ -1,6 +1,7 @@
 namespace System.StateMachine.Pro {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
     using NUnit.Framework;
     using Assert = NUnit.Framework.Assert;
@@ -14,20 +15,26 @@ namespace System.StateMachine.Pro {
                     // machine.SetState State
                     machine.SetRoot( new State(), null, null );
                     Assert.That( machine.Root, Is.Not.Null );
+                    Assert.That( machine.Root.Owner, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.Machine, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.IsRoot, Is.EqualTo( true ) );
                     Assert.That( machine.Root.Root, Is.EqualTo( machine.Root ) );
                     Assert.That( machine.Root.Parent, Is.EqualTo( null ) );
+                    Assert.That( machine.Root.Ancestors.Count(), Is.EqualTo( 0 ) );
+                    Assert.That( machine.Root.AncestorsAndSelf.Count(), Is.EqualTo( 1 ) );
                     Assert.That( machine.Root.Activity, Is.EqualTo( Activity.Active ) );
                 }
                 {
                     // machine.SetState ChildrenableState
                     machine.SetRoot( new ChildrenableState(), null, null );
                     Assert.That( machine.Root, Is.Not.Null );
+                    Assert.That( machine.Root.Owner, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.Machine, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.IsRoot, Is.EqualTo( true ) );
                     Assert.That( machine.Root.Root, Is.EqualTo( machine.Root ) );
                     Assert.That( machine.Root.Parent, Is.EqualTo( null ) );
+                    Assert.That( machine.Root.Ancestors.Count(), Is.EqualTo( 0 ) );
+                    Assert.That( machine.Root.AncestorsAndSelf.Count(), Is.EqualTo( 1 ) );
                     Assert.That( machine.Root.Activity, Is.EqualTo( Activity.Active ) );
                 }
                 {
@@ -45,20 +52,26 @@ namespace System.StateMachine.Pro {
                     // machine.SetState State
                     machine.SetRoot( new State(), null, null );
                     Assert.That( machine.Root, Is.Not.Null );
+                    Assert.That( machine.Root.Owner, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.Machine, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.IsRoot, Is.EqualTo( true ) );
                     Assert.That( machine.Root.Root, Is.EqualTo( machine.Root ) );
                     Assert.That( machine.Root.Parent, Is.EqualTo( null ) );
+                    Assert.That( machine.Root.Ancestors.Count(), Is.EqualTo( 0 ) );
+                    Assert.That( machine.Root.AncestorsAndSelf.Count(), Is.EqualTo( 1 ) );
                     Assert.That( machine.Root.Activity, Is.EqualTo( Activity.Active ) );
                 }
                 {
                     // machine.SetState ChildrenableState
                     machine.SetRoot( new ChildrenableState(), null, null );
                     Assert.That( machine.Root, Is.Not.Null );
+                    Assert.That( machine.Root.Owner, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.Machine, Is.EqualTo( machine ) );
                     Assert.That( machine.Root.IsRoot, Is.EqualTo( true ) );
                     Assert.That( machine.Root.Root, Is.EqualTo( machine.Root ) );
                     Assert.That( machine.Root.Parent, Is.EqualTo( null ) );
+                    Assert.That( machine.Root.Ancestors.Count(), Is.EqualTo( 0 ) );
+                    Assert.That( machine.Root.AncestorsAndSelf.Count(), Is.EqualTo( 1 ) );
                     Assert.That( machine.Root.Activity, Is.EqualTo( Activity.Active ) );
                 }
                 {
